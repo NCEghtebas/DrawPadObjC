5f0d5fab484bc3269b0e5d6a5f79979dd9614bd3
Mandy Korpusik <scope@psis-imac.olin.edu>
2014-10-25 16:25:43 -0400

    more tutorial

---
+++ DrawPad/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  DrawPad
+//
+//  Created by Apportable Scope on 10/25/14.
+//  Copyright (c) 2014 Chloe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
